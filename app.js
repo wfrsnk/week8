5a22e8d5be545ee8e9cdeab89549acd262a2dc63
wfrsnk <57827569+wfrsnk@users.noreply.github.com>
2021-12-13 20:32:46 +0300

    8

--- app.js
+++ app.js
@@ -1,31 +1,30 @@
 
 export default function appScr(express, bodyParser, fs, crypto, http, CORS, User, m) {
     const app = express();
-    const hu = {'Content-Type':'text/html; charset=utf-8',  ...CORS}
-    let headers = {
-        'Content-Type':'text/plain',
-        ...CORS
-    }
-    const headersJSON={'Content-Type':'application/json', ...CORS};
-    const headersCORS = {...CORS};
+    const path = import.meta.url.substring(7);
+    const headersHTML = {'Content-Type':'text/html; charset=utf-8',...CORS}
+    const headersTEXT = {'Content-Type':'text/plain',...CORS}
+    const headersJSON={'Content-Type':'application/json',...CORS}
+    const headersCORS={...CORS}; 
 
-    app
-        .use(bodyParser.urlencoded({extended:true}))       
+    app    
+        .use(bodyParser.urlencoded({extended:true}))  
+        .use(bodyParser.json()) 
         .all('/login/', r => {
-            r.res.set(headers).send('artem_wr');
+            r.res.set(headersTEXT).send('artem_wr');
         })
         .all('/code/', r => {
-            r.res.set(headers)
-            fs.readFile(import.meta.url.substring(7),(err, data) => {
+            r.res.set(headersTEXT)
+            fs.readFile(path,(err, data) => {
                 if (err) throw err;
                 r.res.end(data);
               });           
         })
         .all('/sha1/:input/', r => {
-            r.res.set(headers).send(crypto.createHash('sha1').update(r.params.input).digest('hex'))
+            r.res.set(headersTEXT).send(crypto.createHash('sha1').update(r.params.input).digest('hex'))
         })
         .get('/req/', (req, res) =>{
-            res.set(headers);
+            res.set(headersTEXT);
             let data = '';
             http.get(req.query.addr, async function(response) {
                 await response.on('data',function (chunk){
@@ -35,62 +34,59 @@ export default function appScr(express, bodyParser, fs, crypto, http, CORS, User
             })
         })
         .post('/req/', r =>{
-            r.res.set(headers);
-            const {addr} = req.body;
+            r.res.set(headersTEXT);
+            const {addr} = r.body;
             r.res.send(addr)
         })
+        .all('/wordpress/', r =>{
+            r.res.set(headersJSON).send({
+                id: 1,
+                title: {
+                  rendered: "artem_wr",
+                },
+              });
+        })
+        .all("/wordpress/wp-json/wp/v2/posts/", (r) => {
+          r.res.set(headersJSON).send([{
+            id: 1,
+            title: {
+              rendered: "artem_wr",
+            },
+          }]);
+        })
         .post('/insert/', async r=>{
-            r.res.set(headers);
+            r.res.set(headersTEXT);
             const {login,password,URL}=r.body;
             const newUser = new User({login,password});
             try{
                 await m.connect(URL, {useNewUrlParser:true, useUnifiedTopology:true});
                 try{
                     await newUser.save();
-                    r.res.status(201).json({'Добавлено: ':login});
+                    r.res.status(201).json({'Add: ':login});
                 }
                 catch(e){
-                    r.res.status(400).json({'Ошибка: ':'Нет пароля'});
+                    r.res.status(400).json({'Error: ':'No password'});
                 }
             }
             catch(e){
                 console.log(e.codeName);
             }      
         })
-
-    .all('/wordpress/', r =>{
-        r.res.set(headersJSON).send({
-            id: 1,
-            title: {
-              rendered: "artem_wr",
-            },
-          });
-    })
-    .all("/wordpress/wp-json/wp/v2/posts/", (r) => {
-      r.res.set(headersJSON).send([{
-        id: 1,
-        title: {
-          rendered: "artem_wr",
-        },
-      }]);
-    })
-
-    .all('/render/',async(req,res)=>{
-        res.set(headersCORS);
-        const {addr} = req.query;
-        const {random2, random3} = req.body;
-        
-        http.get(addr,(r, b='') => {
-            r
-            .on('data',d=>b+=d)
-            .on('end',()=>{
-                fs.writeFileSync('views/index.pug', b);
-                res.render('index',{login:'artem_wr',random2,random3})
+        .all('/render/',async(req,res)=>{
+            res.set(headersCORS);
+            const {addr} = req.query;
+            const {random2, random3} = req.body;
+            
+            http.get(addr,(r, b='') => {
+                r
+                .on('data',d=>b+=d)
+                .on('end',()=>{
+                    fs.writeFileSync('views/index.pug', b);
+                    res.render('index',{login:'artem_wr',random2,random3})
+                })
             })
         })
-    })
-    .use(({res:r})=>r.status(404).set(hu).send('artem_wr'))
-    .set('view engine','pug')
-
+        .use(({res:r})=>r.status(404).set(headersHTML).send('artem_wr'))
+        .set('view engine','pug')
     return app;
 }
